fff211cc159afc4b60e5abec13d69ec9507378ae
gusgusz <gustavoguimaraescamps@gmail.com>
2022-12-12 11:51:03 -0300

    showCollections

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from 'cors';
 import pkg from 'pg';
-import categoriesRoutes from './routes/categories.routes.js';
+
  
 const { Pool } = pkg;
 const connectionDb = new Pool({
@@ -15,7 +15,37 @@ const connectionDb = new Pool({
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(categoriesRoutes);
+
+
+
+
+app.get("/categories", async (req, res) => {
+    try {
+        const result = (await connectionDb.query("SELECT * FROM categories")).rows;
+        res.send(result);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+});
+
+app.post("/categories", async (req, res) => {
+    const { name } = req.body;
+    if (!name) {
+        res.sendStatus(400);
+    } else if((await connectionDb.query("SELECT * FROM categories WHERE name = $1", [name])).rows.length > 0) {
+        res.sendStatus(409);
+    }
+    else {
+        try {
+            await connectionDb.query("INSERT INTO categories (name) VALUES ($1)", [name]);
+            res.sendStatus(201);
+        } catch (error) {
+            console.log(error);
+            res.sendStatus(500);
+        }
+    }
+});
 
 const port = 5000;
 app.listen(port, () => console.log(`Server Running in port ${port}`));
--- src/routes/categories.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { showCategories } from "../controllers/categories.controllers.js";
-
-const router = Router();
-
-router.get("/categories", showCategories);
-
-
-export default router;
