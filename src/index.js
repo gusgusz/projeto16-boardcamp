0eb0e48e206b5260fb56d22021c5aa338baed334
gusgusz <gustavoguimaraescamps@gmail.com>
2022-12-13 18:00:23 -0300

    rentals

--- src/index.js
+++ src/index.js
@@ -19,9 +19,14 @@ const gamesSchema = joi.object({
     name: joi.string().min(1).required(),
     phone: joi.string().min(10).max(11).required(),
     cpf: joi.string().min(11).max(11).required(),
-    birthday: joi.date().required(),
+    birthday: joi.date().required()
   });
 
+    const rentalsSchema = joi.object({
+    customerId: joi.number().min(1).integer().required(),
+    gameId: joi.number().min(1).integer().required(),
+    daysRented: joi.number().min(1).required(),
+    });
  
 const { Pool } = pkg;
 const connectionDb = new Pool({
@@ -175,14 +180,16 @@ app.put("/customers/:id", async (req, res) => {
     const { id } = req.params;
     const { name, phone, cpf, birthday } = req.body;
 
-    if (!name || !phone || !cpf || !birthday) {
+    const validation = customersSchema.validate(req.body);
+    if (validation.error) {
+        console.log(validation.error.details[0].message);
         res.sendStatus(400);
-    } else if((await connectionDb.query("SELECT * FROM customers WHERE id = $1", [id])).rows.length > 0) {
+    }else if((await connectionDb.query("SELECT * FROM customers WHERE cpf = $1", [cpf])).rows.length > 0) {
         res.sendStatus(409);
     }
      else {
         try {
-            await connectionDb.query("UPDATE customers SET name = $1, phone = $2, cpf = $3, birthday = $4 WHERE id = $5", [name, phone, cpf, birthday, id]);
+            await connectionDb.query("UPDATE customers SET name = $1, phone = $2, cpf = $3, birthday = $4 WHERE id = $5", [name, phone, cpf, birthday.slice(10), id]);
             res.sendStatus(200);
         } catch (error) {
             console.log(error);
@@ -191,6 +198,107 @@ app.put("/customers/:id", async (req, res) => {
     }
 });
 
+app.get("/rentals", async (req, res) => {
+ 
+    
+    try{
+        const rental = (await connectionDb.query("SELECT * FROM rentals")).rows;
+        
+       let results = [];
+       
+        
+        
+    
+        res.send(rental);
+    }catch(error){
+        console.log(error);
+        res.sendStatus(500);
+    }
+   
+});
+
+app.post("/rentals", async (req, res) => {
+    const { customerId, gameId, daysRented } = req.body;
+    const validation = rentalsSchema.validate(req.body);
+    const rentDate = new Date();
+
+    if (validation.error) {
+        res.sendStatus(400);
+    }
+    
+    const game = (await connectionDb.query("SELECT * FROM games WHERE id = $1", [gameId])).rows;
+    const customer = (await connectionDb.query("SELECT * FROM customers WHERE id = $1", [customerId])).rows;
+   if(game.length === 0 || customer.length === 0 || daysRented <= 0) {
+        res.sendStatus(400);
+    
+   }
+   if(game.stockTotal === 0) {
+        res.sendStatus(400);
+    
+   }
+
+        if(customer.length === 0 || game.length === 0 || daysRented <= 0) {
+            res.sendStatus(400);
+        }
+        const originalPrice = game.pricePerDay * daysRented;
+        const returnDate = null;
+        const delayFee = null;
+        
+        console.log(customerId, gameId, rentDate, daysRented, returnDate, originalPrice, delayFee);
+
+
+        try {
+           
+            await connectionDb.query
+            (`INSERT INTO rentals ("customerId", "gameId", "rentDate", "daysRented", "returnDate", "originalPrice", "delayFee") VALUES ($1, $2, $3, $4, $5, $6, $7)`, [customerId, gameId, rentDate, daysRented, returnDate, originalPrice, delayFee]);
+            res.sendStatus(201);
+        } catch (error) {
+            console.log(error);
+            res.sendStatus(500);
+        }
+    
+});
+app.post("/rentals/:id/return", async (req, res) => {
+    const { id } = req.params;
+    const returnDate = new Date();
+    const rental = (await connectionDb.query("SELECT * FROM rentals WHERE id = $1", [id])).rows[0];
+    if(rental.length === 0) {
+        res.sendStatus(404);
+    }
+    if(rental.returnDate !== null) {
+        res.sendStatus(400);
+    }
+    console.log(rental);
+    const time = Math.floor((rental.rentDate - returnDate)/ 86400000);
+   
+
+  
+        try {
+            if(time > rental.daysRented){
+                const delayFee  = time * (rental.originalPrice/rental.daysRented)
+        
+            
+            await connectionDb.query(
+                `UPDATE rentals SET "delayFee"=$1, "returnDate"=$2 WHERE id=$3`,
+                [delayFee, returnDate, id]
+              );
+              res.sendStatus(200);}
+              else{
+                await connectionDb.query(
+                    `UPDATE rentals SET "returnDate"=$2 WHERE id=$2`,
+                    [returnDate, id]
+                  );
+                  res.sendStatus(200);
+              }
+
+            
+        } catch (error) {
+            console.log(error);
+            res.sendStatus(500);
+        
+    }
+    
+});
 
 const port = 5000;
 app.listen(port, () => console.log(`Server Running in port ${port}`));
